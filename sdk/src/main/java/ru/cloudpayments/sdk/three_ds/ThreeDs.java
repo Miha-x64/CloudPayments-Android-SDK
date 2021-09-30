e9b4c2515ad5f85760a82ed4825e15cf0fb6629a
Miha_x64 <mihlo1996@rambler.ru>
2021-10-01 00:42:54 +0300

    super flexibility

--- sdk/src/main/java/ru/cloudpayments/sdk/three_ds/ThreeDs.java
+++ sdk/src/main/java/ru/cloudpayments/sdk/three_ds/ThreeDs.java
@@ -1,47 +1,40 @@
 package ru.cloudpayments.sdk.three_ds;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.Dialog;
-import android.view.ViewGroup;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
-import android.widget.FrameLayout;
-import androidx.annotation.Nullable;
+import androidx.annotation.CallSuper;
+import androidx.fragment.app.Fragment;
+import org.json.JSONException;
+import org.json.JSONTokener;
+
+import static android.net.Uri.encode;
 
 
 public final class ThreeDs {
     private ThreeDs() {}
 
-    public static Dialog dialog(
-        Activity activity,
-        String acsUrl, String md, String paReq, String termUrl,
-        @Nullable ThreeDSDialogListener listener
-    ) {
-        Dialog dialog = new Dialog(activity);
-        dialog.setContentView(
-            view(activity, acsUrl, md, paReq, termUrl, new ThreeDSDialogListener() {
-                @Override public void onAuthorizationCompleted(String md1, String paRes) {
-                    if (listener != null) listener.onAuthorizationCompleted(md1, paRes);
-                    dialog.dismiss();
-                }
-                @Override public void onAuthorizationFailed(String html) {
-                    if (listener != null) listener.onAuthorizationFailed(html);
-                    dialog.dismiss();
-                }
-            }),
-            new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-        );
-        return dialog;
-    }
-
-    public static WebView view(Activity activity, String acsUrl, String md, String paReq, String termUrl, ThreeDSDialogListener listener) {
-        // view creation itself is left inside fragment in order to minimize diff
-        return ThreeDsDialogFragment.view(activity, acsUrl, md, paReq, termUrl, listener);
+    @SuppressLint("SetJavaScriptEnabled")
+    public static WebView view(Context context) {
+        WebView webViewThreeDs = new WebView(context);
+        webViewThreeDs.getSettings().setDomStorageEnabled(true);
+        webViewThreeDs.getSettings().setJavaScriptEnabled(true);
+        webViewThreeDs.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        return webViewThreeDs;
     }
 
     private static final byte[] PaReq = "PaReq=".getBytes();
     private static final byte[] AndMd = "&MD=".getBytes();
     private static final byte[] AndTermUrl = "&TermUrl=".getBytes();
-    static byte[] requestBody(byte[] paReqVal, byte[] mdVal, byte[] termUrlVal) {
+    public static byte[] requestBody(String paReq, String md, String termUrl) {
+        byte[] paReqVal = encode(paReq).getBytes();
+        byte[] mdVal = encode(md).getBytes();
+        byte[] termUrlVal = encode(termUrl).getBytes();
         byte[] params = new byte[
             PaReq.length + paReqVal.length + AndMd.length + mdVal.length + AndTermUrl.length + termUrlVal.length];
         int pos = 0;
@@ -53,9 +46,124 @@ public final class ThreeDs {
         pos = append(params, pos, termUrlVal);
         return params;
     }
-    static int append(byte[] dst, int dstPos, byte[] src) {
+    private static int append(byte[] dst, int dstPos, byte[] src) {
         System.arraycopy(src, 0, dst, dstPos, src.length);
         return dstPos + src.length;
+    }
+
+    public static class WebViewClient extends android.webkit.WebViewClient {
+        private final Activity activity;
+        private final ThreeDSDialogListener listener;
+        private final String md;
+        private final String termUrl;
+        public WebViewClient(Activity activity, ThreeDSDialogListener listener, String md, String termUrl) {
+            this.activity = activity;
+            this.listener = listener;
+            this.md = md;
+            this.termUrl = termUrl;
+        }
+
+        @CallSuper @Override public void onPageFinished(WebView view, String url) {
+            if (url.equalsIgnoreCase(termUrl)) {
+                view.setVisibility(View.GONE);
+                eval(view, "JSON.parse(document.getElementsByTagName('body')[0].innerText).PaRes",
+                    new ValueCallback<String>() {
+                        @Override public void onReceiveValue(String paRes) {
+                            if (listener != null) {
+                                if (paRes == null) {
+                                    eval(view, "document.getElementsByTagName('html')[0].innerHTML",
+                                        new ValueCallback<String>() {
+                                            @Override public void onReceiveValue(String s) {
+                                                listener.onAuthorizationFailed(s);
+                                            }
+                                        }
+                                    );
+                                } else {
+                                    listener.onAuthorizationCompleted(md, paRes);
+                                }
+                            }
+                        }
+                    }
+                );
+            }
+        }
+        private void eval(WebView view, String script, ValueCallback<String> callback) {
+            view.evaluateJavascript(script, new ValueCallback<String>() {
+                @Override public void onReceiveValue(final String jsString) {
+                    String unquoted = null;
+                    try {
+                        Object parsed;
+                        if (jsString != null && !jsString.isEmpty() &&
+                            ((parsed = new JSONTokener(jsString).nextValue()) instanceof String) &&
+                            !((String) parsed).isEmpty())
+                            unquoted = (String) parsed;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    final String finalUnquoted = unquoted;
+                    activity.runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            callback.onReceiveValue(finalUnquoted);
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    public static final class DialogFragmentResultListener implements ThreeDSDialogListener {
+        private final DialogInterface dialog;
+        private final Fragment fragment;
+        private final ThreeDSDialogListener delegate;
+        public DialogFragmentResultListener(DialogInterface dialog, Fragment fragment, ThreeDSDialogListener delegate) {
+            this.dialog = dialog;
+            this.fragment = fragment;
+            this.delegate = delegate;
+        }
+
+        @Override public void onAuthorizationCompleted(String md, String paRes) {
+            String rk;
+            if (fragment != null && (rk = fragment.requireArguments().getString("rk")) != null)
+                deliver(rk, md, paRes, null);
+
+            if (delegate != null)
+                delegate.onAuthorizationCompleted(md, paRes);
+
+            if (dialog != null)
+                dialog.dismiss();
+        }
+        @Override public void onAuthorizationFailed(String html) {
+            String rk;
+            if (fragment != null && (rk = fragment.requireArguments().getString("rk")) != null)
+                deliver(rk, null, null, html == null || html.length() > 128 * 1024 ? null : html);
+            //                                        just a safe guess ^^^^^^^^^^
+            if (delegate != null)
+                delegate.onAuthorizationFailed(html);
+
+            if (dialog != null)
+                dialog.dismiss();
+        }
+        private void deliver(String rk, String md, String paRes, String html) {
+            Bundle bun = new Bundle(2);
+            if (md != null) {
+                bun.putString("md", md);
+                bun.putString("paRes", paRes);
+            } else if (html != null) {
+                bun.putString("html", html);
+            } // else error HTML was truncated
+
+            Bundle args = fragment.requireArguments();
+            if (fragment.isAdded()) fragment.getParentFragmentManager().setFragmentResult(rk, bun);
+            else args.putBundle("result", bun);
+        }
+
+        public static boolean tryDeliver(Fragment who) {
+            Bundle args = who.requireArguments();
+            Bundle result = args.getBundle("result");
+            boolean deliver = result != null;
+            if (deliver) who.getParentFragmentManager().setFragmentResult(args.getString("rk"), result);
+            return deliver;
+        }
     }
 
 }
--- sdk/src/main/java/ru/cloudpayments/sdk/three_ds/ThreeDsDialogFragment.java
+++ sdk/src/main/java/ru/cloudpayments/sdk/three_ds/ThreeDsDialogFragment.java
@@ -1,26 +1,23 @@
 package ru.cloudpayments.sdk.three_ds;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.webkit.ValueCallback;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import org.json.JSONException;
-import org.json.JSONTokener;
 
-import static android.net.Uri.encode;
-import static ru.cloudpayments.sdk.three_ds.ThreeDs.requestBody;
+public class ThreeDsDialogFragment extends DialogFragment implements DialogInterface {
 
-public class ThreeDsDialogFragment extends DialogFragment {
-
-    private ThreeDSDialogListener listener;
+    protected ThreeDSDialogListener listener;
+    protected WebView webView;
 
     @Deprecated public ThreeDsDialogFragment() {
     }
@@ -40,56 +37,37 @@ public class ThreeDsDialogFragment extends DialogFragment {
         return this;
     }
 
-    static WebView view(Activity activity, String acsUrl, String md, String paReq, String termUrl, ThreeDSDialogListener listener) {
-        WebView webViewThreeDs = new WebView(activity);
-        webViewThreeDs.setWebViewClient(new ThreeDsDialogFragment.ThreeDsWebViewClient(activity, listener, md, termUrl));
-        webViewThreeDs.getSettings().setDomStorageEnabled(true);
-        webViewThreeDs.getSettings().setJavaScriptEnabled(true);
-        webViewThreeDs.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+    @Nullable @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        String[] args = requireArguments().getStringArray("args");
+        String acsUrl = args[0];
+        String md = args[1];
+        String paReq = args[2];
+        String termUrl = args[3];
 
-        webViewThreeDs.postUrl(
-            acsUrl,
-            requestBody(encode(paReq).getBytes(), encode(md).getBytes(), encode(termUrl).getBytes())
-        );
-        return webViewThreeDs;
+        Activity activity = requireActivity();
+        (webView = ThreeDs.view(activity)).setWebViewClient(new ThreeDs.WebViewClient(
+            activity, new ThreeDs.DialogFragmentResultListener(this, this, listener), md, termUrl
+        ));
+
+        if (savedInstanceState == null)
+            webView.postUrl(acsUrl, ThreeDs.requestBody(paReq, md, termUrl));
+        else
+            webView.restoreState(savedInstanceState);
+
+        return webView;
     }
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Bundle bun = requireArguments();
-        String[] args = bun.getStringArray("args");
-        String rk = bun.getString("rk");
-        return ThreeDs.dialog(
-            requireActivity(),
-            args[0], args[1], args[2], args[3],
-            rk == null ? listener : new ThreeDSDialogListener() {
-                @Override public void onAuthorizationCompleted(String md, String paRes) {
-                    deliver(md, paRes, null);
-                    if (listener != null) listener.onAuthorizationCompleted(md, paRes);
-                }
-                @Override public void onAuthorizationFailed(String html) {
-                    deliver(null, null, html == null || html.length() > 128 * 1024 ? null : html);
-                    //                                just a safe guess ^^^^^^^^^^
-                    if (listener != null) listener.onAuthorizationFailed(html);
-                }
-                private void deliver(String md, String paRes, String html) {
-                    Bundle bun = new Bundle(2);
-                    if (md != null) {
-                        bun.putString("md", md);
-                        bun.putString("paRes", paRes);
-                    } else if (html != null) {
-                        bun.putString("html", html);
-                    } // else error HTML was truncated
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+    }
 
-                    if (isAdded()) {
-                        getParentFragmentManager().setFragmentResult(rk, bun);
-                        // and a decorator from ThreeDs.dialog() will dismiss us
-                    } else {
-                        requireArguments().putBundle("result", bun);
-                    }
-                }
-            }
-        );
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        webView = null;
     }
 
     @Override
@@ -100,72 +78,8 @@ public class ThreeDsDialogFragment extends DialogFragment {
             window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         }
 
-        Bundle args = requireArguments();
-        Bundle result = args.getBundle("result");
-        if (result != null) {
-            getParentFragmentManager().setFragmentResult(args.getString("rk"), result);
+        if (ThreeDs.DialogFragmentResultListener.tryDeliver(this)) {
             dismiss();
-        }
-    }
-
-    static final class ThreeDsWebViewClient extends WebViewClient {
-        private final Activity activity;
-        private final ThreeDSDialogListener listener;
-        private final String md;
-        private final String termUrl;
-        ThreeDsWebViewClient(Activity activity, ThreeDSDialogListener listener, String md, String termUrl) {
-            this.activity = activity;
-            this.listener = listener;
-            this.md = md;
-            this.termUrl = termUrl;
-        }
-
-        @Override public void onPageFinished(WebView view, String url) {
-            if (url.equalsIgnoreCase(termUrl)) {
-                view.setVisibility(View.GONE);
-                eval(view, "JSON.parse(document.getElementsByTagName('body')[0].innerText).PaRes",
-                    new ValueCallback<String>() {
-                        @Override public void onReceiveValue(String paRes) {
-                            if (listener != null) {
-                                if (paRes == null) {
-                                    eval(view, "document.getElementsByTagName('html')[0].innerHTML",
-                                        new ValueCallback<String>() {
-                                            @Override public void onReceiveValue(String s) {
-                                                listener.onAuthorizationFailed(s);
-                                            }
-                                        }
-                                    );
-                                } else {
-                                    listener.onAuthorizationCompleted(md, paRes);
-                                }
-                            }
-                        }
-                    }
-                );
-            }
-        }
-
-        private void eval(WebView view, String script, ValueCallback<String> callback) {
-            view.evaluateJavascript(script, new ValueCallback<String>() {
-                @Override public void onReceiveValue(final String jsString) {
-                    String unquoted = null;
-                    try {
-                        Object parsed;
-                        if (jsString != null && !jsString.isEmpty() &&
-                            ((parsed = new JSONTokener(jsString).nextValue()) instanceof String) &&
-                            !((String) parsed).isEmpty())
-                            unquoted = (String) parsed;
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    final String finalUnquoted = unquoted;
-                    activity.runOnUiThread(new Runnable() {
-                        @Override public void run() {
-                            callback.onReceiveValue(finalUnquoted);
-                        }
-                    });
-                }
-            });
         }
     }
 
@@ -181,11 +95,14 @@ public class ThreeDsDialogFragment extends DialogFragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             if (activity instanceof ThreeDSDialogListener) {
                 listener = (ThreeDSDialogListener) activity;
             }
         }
     }
+
+    // In DialogInterface, we mind only dismiss() (which is already implemented).
+    @Override public void cancel() {}
+
 }
